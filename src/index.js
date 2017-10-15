cb3ace8b89e60db682a3adf89ef66c3e6ed8c627
antoniogiordano <antonio.giordano@getapper.com>
2017-10-15 22:28:29 +0200

    minor fix

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import AttireJoi from './Attire'
 export { validationsStates } from './joi'
 export { transform } from './transform'
 export { value } from './value'
-export { AttireJoi }
 export default AttireJoi
